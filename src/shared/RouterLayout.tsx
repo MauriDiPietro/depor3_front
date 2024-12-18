68fdfe627aff9890eff8dab056283ecca3189091
MauriDiPietro <dipietro.jm@gmail.com>
2024-12-18 08:30:57 -0300

    ajuste import no utilizado

--- src/shared/RouterLayout.tsx
+++ src/shared/RouterLayout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavBar } from "./NavBar";
+// import { NavBar } from "./NavBar";
 import { Outlet } from "react-router-dom";
 
 export const RouterLayout: React.FC<{}> = () => {
