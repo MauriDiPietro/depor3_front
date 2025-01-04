6e203d5dadd6feb4dd380cbdabc4cb853de31361
MauriDiPietro <dipietro.jm@gmail.com>
2025-01-04 08:57:48 -0300

    seccion entrevistas

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import { RouterLayout } from "./shared/RouterLayout";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { NewsDetail } from "./pages/NewDetail/new-detail";
+import Entrevistas from "./pages/Entrevistas";
 
 export const AppRouter: React.FC<{}> = () => {
   return (
@@ -12,6 +13,7 @@ export const AppRouter: React.FC<{}> = () => {
       <Route path="/" element={<RouterLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/news/:id" element={<NewsDetail />} />
+        <Route path="/entrevistas" element={<Entrevistas/>} />
       </Route>
       {/* rutas con navbar */}
       <Route path="/login" element={<Login />} />
---
+++ src/pages/Entrevistas/index.tsx
@@ -0,0 +1,13 @@
+
+const Entrevistas = () => {
+  return (
+    <>
+    <br />
+    <br />
+    <iframe allowFullScreen className="fp-iframe" style={{border: "1px solid lightgray;", width: "100%", height: "400px"}} src="https://heyzine.com/flip-book/7900c8ed6c.html"></iframe>
+    </>
+  )
+}
+
+export default Entrevistas
+
