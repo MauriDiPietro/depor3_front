a11b3c9c90386c1b62879e8dcf9ffbdb4140355c
MauriDiPietro <dipietro.jm@gmail.com>
2025-02-01 08:21:55 -0300

    codigos qr

--- src/Router.tsx
+++ src/Router.tsx
@@ -16,8 +16,28 @@ export const AppRouter: React.FC<{}> = () => {
         <Route path="/" element={<Home />} />
         {/* Redirecciones */}
         <Route
+          path="/deportistas_miguel-marin"
+          element={<Navigate to="/news/67951df99130785a0c518e03" replace />}
+        />
+        <Route
+          path="/deportistas_ivanna-madruga"
+          element={<Navigate to="/news/67951daf9130785a0c518e02" replace />}
+        />
+        <Route
+          path="/deportistas_caco-colla"
+          element={<Navigate to="/news/67951d829130785a0c518e00" replace />}
+        />
+        <Route
           path="/deportistas_piojo-lopez"
           element={<Navigate to="/news/67951d9c9130785a0c518e01" replace />}
+        />
+        <Route
+          path="/deportistas_curita-peralta"
+          element={<Navigate to="/news/67951d5b9130785a0c518dff" replace />}
+        />
+        <Route
+          path="/nuestro-gen-dominante"
+          element={<Navigate to="/news/679d4d00defad880b9ece1b6" replace />}
         />
         <Route path="/news/:id" element={<NewsDetail />} />
         <Route path="/entrevistas" element={<Entrevistas />} />
