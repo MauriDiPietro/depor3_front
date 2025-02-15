36ba4f735d89b2075d99fba464935c92cb9dabbc
MauriDiPietro <dipietro.jm@gmail.com>
2025-02-15 15:54:23 -0300

    fix: links patio qr

--- src/Router.tsx
+++ src/Router.tsx
@@ -21,27 +21,67 @@ export const AppRouter: React.FC<{}> = () => {
           {/* Redirecciones */}
           <Route
             path="/deportistas_miguel-marin"
-            element={<Navigate to="/news/67951df99130785a0c518e03" replace />}
+            element={<Navigate to="/patio/67951df99130785a0c518e03" replace />}
           />
           <Route
             path="/deportistas_ivanna-madruga"
-            element={<Navigate to="/news/67951daf9130785a0c518e02" replace />}
+            element={<Navigate to="/patio/67951daf9130785a0c518e02" replace />}
           />
           <Route
             path="/deportistas_caco-colla"
-            element={<Navigate to="/news/67951d829130785a0c518e00" replace />}
+            element={<Navigate to="/patio/67951d829130785a0c518e00" replace />}
           />
           <Route
             path="/deportistas_piojo-lopez"
-            element={<Navigate to="/news/67951d9c9130785a0c518e01" replace />}
+            element={<Navigate to="/patio/67951d9c9130785a0c518e01" replace />}
           />
           <Route
             path="/deportistas_curita-peralta"
-            element={<Navigate to="/news/67951d5b9130785a0c518dff" replace />}
+            element={<Navigate to="/patio/67951d5b9130785a0c518dff" replace />}
           />
           <Route
             path="/nuestro-gen-dominante"
-            element={<Navigate to="/news/679d4d00defad880b9ece1b6" replace />}
+            element={<Navigate to="/patio/679d4d00defad880b9ece1b6" replace />}
+          />
+          <Route
+            path="/deportistas_andrea-berrino"
+            element={<Navigate to="/patio/67b094f94327ddfef1f3c85b" replace />}
+          />
+          <Route
+            path="/deportistas_gusti-fernandez"
+            element={<Navigate to="/patio/67b0972c09ea7a8ef50b99ea" replace />}
+          />
+          <Route
+            path="/deportistas_oscar-galindez"
+            element={<Navigate to="/patio/67b094d0cfdbed00563833dd" replace />}
+          />
+          <Route
+            path="/deportistas_marcelo-mario-milanesio"
+            element={<Navigate to="/patio/67b0949f4327ddfef1f3c852" replace />}
+          />
+          <Route
+            path="/deportistas_pechito-lopez"
+            element={<Navigate to="/patio/67b0943b09ea7a8ef50b99d4" replace />}
+          />
+          <Route
+            path="/deportistas_gustavo-fernandez"
+            element={<Navigate to="/patio/67b092504327ddfef1f3c845" replace />}
+          />
+          <Route
+            path="/deportistas_alfredo-altamirano"
+            element={<Navigate to="/patio/67b08def09ea7a8ef50b99a4" replace />}
+          />
+          <Route
+            path="/deportistas_juan-manuel-fernandez"
+            element={<Navigate to="/patio/67b08fa009ea7a8ef50b99b9" replace />}
+          />
+          <Route
+            path="/deportistas_rocio-comba"
+            element={<Navigate to="/patio/67a8801c22908d6c35011f94" replace />}
+          />
+          <Route
+            path="/deportistas_gustavo-pascutti"
+            element={<Navigate to="/patio/67ab67b31558b59f0defec8d" replace />}
           />
           <Route path="/news/:id" element={<NewsDetail />} />
           <Route path="/draft/:id" element={<DraftsDetail />} />
