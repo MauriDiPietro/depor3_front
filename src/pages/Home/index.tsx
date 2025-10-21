dc02e74a5121cb06c01e6587ced78bec55ed74d2
MauriDiPietro <dipietro.jm@gmail.com>
2025-10-21 12:23:50 -0300

    publi gobcba

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -34,25 +34,23 @@ export const Home: React.FC<{}> = () => {
           </Box>
         </Grid>
         <Grid item xs={12} md={8}>
-          {/*
-            <Box>
-              <a
-                href="https://youtu.be/h2J_ZdbxYVc"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src="https://res.cloudinary.com/dsooxiydo/image/upload/v1743853097/gob-cba-04-2025.gif"
-                  alt="Publicidad"
-                  style={{
-                    width: "100%",
-                    maxWidth: "700px",
-                    height: "auto",
-                  }}
-                />
-              </a>
-            </Box>
-          */}
+          <Box>
+            <a
+              href="https://www.cba.gov.ar/dengue/?utm_source=DIGITAL&utm_medium=RRSS&utm_campaign=Dengue26xSdC&utm_id=Dengue26"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://res.cloudinary.com/dsooxiydo/image/upload/v1743853097/gob-cba-04-2025.gif"
+                alt="Publicidad"
+                style={{
+                  width: "100%",
+                  maxWidth: "700px",
+                  height: "auto",
+                }}
+              />
+            </a>
+          </Box>
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={12}>
           <NewsGrid />
