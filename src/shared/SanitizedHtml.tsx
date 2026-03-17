06e58b8cfb0f22f5166cbe7a8cdc0240c2b7a917
MauriDiPietro <dipietro.jm@gmail.com>
2026-03-17 11:59:47 -0300

    feat: publi muni

--- src/shared/SanitizedHtml.tsx
+++ src/shared/SanitizedHtml.tsx
@@ -199,7 +199,7 @@ export const SanitizedHtml: React.FC<SanitizedHtmlProps> = ({
     const imageUrls = [
       "https://res.cloudinary.com/dsooxiydo/image/upload/v1767369200/muni_almafuerte_01_2026.jpg",
       "https://res.cloudinary.com/dsooxiydo/image/upload/v1751469404/ferretti.jpg",
-      "https://res.cloudinary.com/dsooxiydo/image/upload/v1768215972/muni_rio3_01_2026.jpg",
+      "https://res.cloudinary.com/dsooxiydo/image/upload/v1773759551/muni_rio3_03-2026_k95tjs.png",
       // "https://res.cloudinary.com/dsooxiydo/image/upload/v1737143687/capital-deportista.jpg",
       rotarPubliCoop(),
       "https://res.cloudinary.com/dsooxiydo/image/upload/v1751470853/HRT_tzoqml.png",
