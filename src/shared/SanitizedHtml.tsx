03ae2ed9aa60e80c128ba4d53d6bc078c164b436
MauriDiPietro <dipietro.jm@gmail.com>
2026-07-31 08:24:46 -0300

    feta: publi coop

--- src/shared/SanitizedHtml.tsx
+++ src/shared/SanitizedHtml.tsx
@@ -183,25 +183,26 @@ export const SanitizedHtml: React.FC<SanitizedHtmlProps> = ({
     const parser = new DOMParser(); 
     const doc = parser.parseFromString(htmlString, "text/html");
 
-    const publiCoopRotativa = [
-      "https://res.cloudinary.com/dsooxiydo/image/upload/v1752581147/Evolucionando_92A_DepoR3_F_Negro_wk6nmo.jpg",
-      "https://res.cloudinary.com/dsooxiydo/image/upload/v1752581100/Evolucionando_92A_DepoR3_F_Blanco_ltbufg.jpg",
-    ];
+    // const publiCoopRotativa = [
+    //   "https://res.cloudinary.com/dsooxiydo/image/upload/v1752581147/Evolucionando_92A_DepoR3_F_Negro_wk6nmo.jpg",
+    //   "https://res.cloudinary.com/dsooxiydo/image/upload/v1752581100/Evolucionando_92A_DepoR3_F_Blanco_ltbufg.jpg",
+    // ];
 
-    let publiCoopIndex = 0;
+    // let publiCoopIndex = 0;
 
-    const rotarPubliCoop = () => {
-      const url = publiCoopRotativa[publiCoopIndex];
-      publiCoopIndex = (publiCoopIndex + 1) % publiCoopRotativa.length;
-      return url;
-    };
+    // const rotarPubliCoop = () => {
+    //   const url = publiCoopRotativa[publiCoopIndex];
+    //   publiCoopIndex = (publiCoopIndex + 1) % publiCoopRotativa.length;
+    //   return url;
+    // };
 
     const imageUrls = [
+      "https://res.cloudinary.com/dsooxiydo/image/upload/v1785496956/Construyendo_Futuro_Juntos.jpg_muyqos.jpg", //COOP RIO 3
+      "https://res.cloudinary.com/dsooxiydo/image/upload/v1776773351/GR%C3%81FICA_WEB_1200_x_150_px_omv0ij.png", //MUNI-RIO-3
       "https://res.cloudinary.com/dsooxiydo/image/upload/v1767369200/muni_almafuerte_01_2026.jpg",  //MUNI-ALMA
       "https://res.cloudinary.com/dsooxiydo/image/upload/v1751469404/ferretti.jpg",
-      "https://res.cloudinary.com/dsooxiydo/image/upload/v1776773351/GR%C3%81FICA_WEB_1200_x_150_px_omv0ij.png", //MUNI-RIO-3
       // "https://res.cloudinary.com/dsooxiydo/image/upload/v1737143687/capital-deportista.jpg",
-      rotarPubliCoop(),
+      // rotarPubliCoop(),
       "https://res.cloudinary.com/dsooxiydo/image/upload/v1751470853/HRT_tzoqml.png",
       "https://res.cloudinary.com/dsooxiydo/image/upload/v1751470853/MG_el54z1.png",
       "https://res.cloudinary.com/dsooxiydo/image/upload/v1737143687/aceros-rio.jpg",
